1f111bae76e60df54d4ebdc7b294614873f016d3
Enzo Zerega <eczerega@MacBook-Pro-de-Enzo-2.local>
2016-03-17 01:59:24 -0300

    avance

--- app/models/socials_need_image.rb
+++ app/models/socials_need_image.rb
@@ -1,3 +1,11 @@
 class SocialsNeedImage < ActiveRecord::Base
 	belongs_to :socials_need
+  	has_attached_file :image_url {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+  }
+
+	  # Validate the attached image is image/jpg, image/png, etc
+	  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
